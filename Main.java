749cade078ff6e936b569a94482071f24eee9654
STI_LaurenceJeff_ATIENZA <125817655+SerialLoliman@users.noreply.github.com>
2023-03-18 15:19:46 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
-
 package animals;
 public class Main {
     public static void main(String[] args) {
        
-        Falcon RedFalcon = new Falcon(110);
-        Hippopotamus BlackHippopotamus = new Hippopotamus(2000);
-        Giraffe PinkGiraffe = new Giraffe(600);
+        Falcon RedFalcon = new Falcon(110, "Falcon");
+        Hippopotamus BlackHippopotamus = new Hippopotamus(2000, "Hippopotamus");
+        Giraffe PinkGiraffe = new Giraffe(600, "Giraffe");
         
         RedFalcon.fly();
         RedFalcon.showenergy();
@@ -19,9 +18,5 @@ public class Main {
         PinkGiraffe.showenergy();
         PinkGiraffe.sleep();
         PinkGiraffe.showenergy();
-        
-        
-        
     }
-    
 }
