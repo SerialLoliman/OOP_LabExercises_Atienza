d0b8be67095791c5c1989efdc3a9b6d99cd8e2ce
STI_LaurenceJeff_ATIENZA <125817655+SerialLoliman@users.noreply.github.com>
2023-03-18 15:20:43 +0800

    Update Giraffe.java

--- Giraffe.java
+++ Giraffe.java
@@ -1,26 +1,18 @@
-
 package animals;
-
-
 public class Giraffe extends Animals{
-
-    public Giraffe(int energy) {
-        super(energy);
-    }
+    public Giraffe(int energy, String name) {
+        super(energy, name);}
     
     public void stretch(){
-    System.out.println("This Giraffe stretched");
-    energy -= 5;
-    }
+    System.out.println("This " + this.name + " stretched");
+    energy -= 5;}
 
     public void run(){
-    System.out.println("This Giraffe ran");
-    energy -= 15;
-    }
+    System.out.println("This " + this.name + " ran");
+    energy -= 15;}
     
     public void gallop(){
-    System.out.println("This Giraffe galloped");
-    energy -= 20;
-    }
+    System.out.println("This " + this.name + " galloped");
+    energy -= 20;}
 }
 
