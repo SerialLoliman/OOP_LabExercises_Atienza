1097eb76e5333db55567d9f4afd726c0de3550e7
STI_LaurenceJeff_ATIENZA <125817655+SerialLoliman@users.noreply.github.com>
2023-03-18 15:23:04 +0800

    Update Falcon.java

--- Falcon.java
+++ Falcon.java
@@ -1,23 +1,18 @@
-
 package animals;
 public class Falcon extends Animals{
-
-    public Falcon(int energy) {
-        super(energy);
-    }
+    public Falcon(int energy, String name) {
+        super(energy, name);
+        this.name = "Falcon";}
     
     public void fly(){
-    System.out.println("This Falcon Flew");
-    energy -= 10;
-    }
+    System.out.println("This " + this.name + " Flew");
+    energy -= 10;}
 
     public void swoop(){
-    System.out.println("This Falcon Swooped");
-    energy -= 15;
-    }
+    System.out.println("This " + this.name + " Swooped");
+    energy -= 15;}
     
     public void flap(){
-    System.out.println("This Falcon Flapped");
-    energy -= 5;
-    }
+    System.out.println("This " + this.name + " Flapped");
+    energy -= 5;}
 }
