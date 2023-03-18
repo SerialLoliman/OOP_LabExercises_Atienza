b36e0c7309d66839fdb0b2fde553c61449e52db7
STI_LaurenceJeff_ATIENZA <125817655+SerialLoliman@users.noreply.github.com>
2023-03-18 15:21:19 +0800

    Update Hippopotamus.java

--- Hippopotamus.java
+++ Hippopotamus.java
@@ -1,24 +1,17 @@
-
 package animals;
-
 public class Hippopotamus extends Animals{
-
-    public Hippopotamus(int energy) {
-        super(energy);
-    }
+    public Hippopotamus(int energy, String name) {
+        super(energy, name);}
     
     public void bellyflop(){
-    System.out.println("This Hippopotamus flopped it's belly");
-    energy -= 20;
-    }
+    System.out.println("This " + this.name + " flopped it's belly");
+    energy -= 20;}
 
     public void squirt(){
-    System.out.println("This Hippopotamus Squirted");
-    energy -= 5;
-    }
+    System.out.println("This " + this.name + " Squirted");
+    energy -= 5;}
     
     public void swim(){
-    System.out.println("This Hippopotamus swam");
-    energy -= 15;
-    }
+    System.out.println("This " + this.name + " swam");
+    energy -= 15;}
 }
